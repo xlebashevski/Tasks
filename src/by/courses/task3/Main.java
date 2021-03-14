bf65d3727247b2ed31d2e5c02204b9655372c93d
Hleb_Mikhnevich <xlebashevski1996@mail.ru>
2021-03-14 21:43:07 +0300

    Update logic task 3

--- src/by/courses/task3/Main.java
+++ src/by/courses/task3/Main.java
@@ -2,10 +2,12 @@ package by.courses.task3;
 
 public class Main {
     public static void main(String[] args) {
-        int array[] = {1, 2, 3, 6, 5, 21, 5};
+        int array[] = {1, 2, 3, 4, 5, 6, 7};
+        int array_1[] = new int [array.length];
         for (int i = array.length - 1; i < array.length; i--) {
             if (i >= 0) {
-                System.out.print(array[i] + " ");
+                array_1[i] = array[i];
+                System.out.print(array_1[i] + " ");
             }
         }
     }
